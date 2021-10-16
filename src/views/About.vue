373db59729574879bf2015f756a7f97634fc3327
israel <aimorbar@gmail.com>
2021-10-17 00:02:09 +0200

    primera subida clase 126

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>Contador : {{contador}}</h1>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
+
+  </div>
+</template>
+<script>
+import {ref} from "vue";
+export default {
+  //Este metodo es llamado antes de que se pinte el componente
+  setup(){
+    //esta variable actua como referencia a un valor para que se pueda
+    //modificar su valor de forma reactiva (Sin refrescar la pantalla)
+    let contador  = ref(0);
+    const aumentar = () => {
+      contador.value ++
+    }
+    const disminuir = () => {
+      contador.value --
+    }
+    return {
+      contador,
+      aumentar,
+      disminuir
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+<h1>contador : {{contador}}</h1>
+    <button @click="aumentar">aumentar</button>
+    <button @click="disminuir">disminuir</button>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  data() {
+    return {
+      contador: 0
+
+    }
+  },
+  methods:{
+    aumentar(){
+      this.contador++
+    },
+    disminuir(){
+      this.contador--
+    }
+  }
+
+}
+</script>
